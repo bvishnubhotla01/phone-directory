9ed2fd8916eadd5670d8a19defb10f6300fca1e8
Balaji Vishnubhotla <balaji.vishnubhotla@gmail.com>
2021-01-30 21:17:17 +0530

    CReated Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-      Phone Directory
+        <Header />
+        <button>Add</button>
+        <div>
+          <span>Name</span> <br />
+          <span>Phone</span>
+        </div>
       </div>
     );
-  }  
+  }
 }
 
 export default App;
